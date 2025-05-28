1f11c84df87a26d930aa55f27c170aa1a3cbb6ce
qoidnaufal <qoidnaufal@gmail.com>
2025-05-29 02:04:12 +0700

    experimental vecmap

--- crates/aplite_storage/src/lib.rs
+++ crates/aplite_storage/src/lib.rs
@@ -1,8 +1,20 @@
 use std::marker::PhantomData;
 
 #[derive(Debug)]
-pub enum SlotContent<V> {
+pub struct MaxCapacityReached;
+
+impl std::fmt::Display for MaxCapacityReached {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl std::error::Error for MaxCapacityReached {}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum SlotContent<V> {
     Occupied(V),
+    // contains index of next free slot
     Vacant(u32),
 }
 
@@ -20,40 +32,61 @@ impl<V> SlotContent<V> {
             SlotContent::Vacant(_) => None,
         }
     }
+
+    fn swap(&mut self, idx: u32) -> Option<V> {
+        let swap = std::mem::replace(self, Self::Vacant(idx));
+        if let SlotContent::Occupied(v) = swap {
+            Some(v)
+        } else {
+            None
+        }
+    }
 }
 
-#[derive(Debug)]
-pub struct Slot<V> {
+#[derive(Debug, Clone)]
+struct Slot<V> {
     content: SlotContent<V>,
-    next: u32,
+    version: u32,
 }
 
 impl<V> Slot<V> {
-    fn vacant(idx: u32) -> Self {
+    fn vacant(pref_free_slot: u32) -> Self {
         Self {
-            content: SlotContent::Vacant(idx),
-            next: idx + 1,
+            content: SlotContent::Vacant(pref_free_slot),
+            version: 0,
         }
     }
 
-    fn occupied(v: V, current: u32) -> Self {
+    fn occupied(v: V) -> Self {
         Self {
             content: SlotContent::Occupied(v),
-            next: current + 1,
+            version: 0,
         }
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-pub struct KeyHandle<K: Key> {
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct Key<K: KeyID> {
     idx: u32,
+    version: u32,
     phantom: PhantomData<K>,
 }
 
-impl<K: Key> KeyHandle<K> {
-    fn new(idx: u32) -> Self {
+impl<K: KeyID> std::fmt::Debug for Key<K> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let type_name = std::any::type_name::<K>()
+            .split("::")
+            .last()
+            .unwrap();
+        write!(f, "{} {{ version: {:03?} }}", type_name, self.version)
+    }
+}
+
+impl<K: KeyID> Key<K> {
+    fn new(idx: u32, version: u32) -> Self {
         Self {
             idx,
+            version,
             phantom: PhantomData,
         }
     }
@@ -61,20 +94,43 @@ impl<K: Key> KeyHandle<K> {
     pub fn idx(&self) -> u32 { self.idx }
 }
 
-#[derive(Debug)]
-pub struct Map<K: Key, V> {
+pub trait KeyID: Sized {}
+
+#[derive(Clone)]
+pub struct VecMap<K: KeyID, V> {
     inner: Vec<Slot<V>>,
     free_slot: u32,
     count: u32,
     phantom: PhantomData<K>,
 }
 
-pub trait Key: Sized {
-    fn idx(&self) -> u32;
-    fn from_handle(handle: &KeyHandle<Self>) -> Self;
+impl<K: KeyID, V: std::fmt::Debug> std::fmt::Debug for VecMap<K, V> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.count == 0 {
+            write!(f, "[]")
+        } else {
+            let to_write = self
+                .inner
+                .iter()
+                .filter_map(|slot| {
+                    match &slot.content {
+                        SlotContent::Occupied(v) => Some(v),
+                        SlotContent::Vacant(_) => None,
+                    }
+                })
+                .collect::<Vec<_>>();
+            write!(f, "{to_write:?}")
+        }
+    }
 }
 
-impl<K: Key, V> Map<K, V> {
+impl<K: KeyID, V> Default for VecMap<K, V> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<K: KeyID, V> VecMap<K, V> {
     pub fn new() -> Self {
         Self::new_with_capacity(0)
     }
@@ -85,7 +141,7 @@ impl<K: Key, V> Map<K, V> {
 
     fn new_with_capacity(capacity: usize) -> Self {
         let mut inner = Vec::with_capacity(capacity + 1);
-        let slot = Slot::vacant(0);
+        let slot = Slot::vacant(1);
         inner.push(slot);
         Self {
             inner,
@@ -95,92 +151,198 @@ impl<K: Key, V> Map<K, V> {
         }
     }
 
-    pub fn insert(&mut self, v: V) -> K {
-        if self.count + 1 == u32::MAX { panic!("reached max capacity") }
+    pub fn len(&self) -> usize { self.count as usize }
 
-        self.try_insert_with_key(|h| Ok((K::from_handle(h), v))).unwrap()
+    pub fn is_empty(&self) -> bool { self.count == 0 }
+
+    pub fn insert(&mut self, v: V) -> Key<K> {
+        self.insert_with_key(|_| Ok(v))
     }
 
-    pub fn insert_with_key<F>(&mut self, f: F) -> K
-    where F: FnOnce(&KeyHandle<K>) -> Result<(K, V), ()>
-    { self.try_insert_with_key(f).unwrap() }
-
-    pub fn try_insert_with_key<F>(&mut self, f: F) -> Result<K, ()>
+    pub fn insert_with_key<F>(&mut self, f: F) -> Key<K>
     where
-        F: FnOnce(&KeyHandle<K>) -> Result<(K, V), ()>,
+        F: FnOnce(&Key<K>) -> Result<V, MaxCapacityReached>
     {
-        if self.count + 1 == u32::MAX { panic!("reached max capacity") }
+        self.try_insert_with_key(f).unwrap()
+    }
 
+    pub fn try_insert_with_key<F>(&mut self, f: F) -> Result<Key<K>, MaxCapacityReached>
+    where
+        F: FnOnce(&Key<K>) -> Result<V, MaxCapacityReached>,
+    {
+        if self.count + 1 == u32::MAX { return Err(MaxCapacityReached) }
+
+        // check vacant spot first
         if let Some(slot) = self.inner.get_mut(self.free_slot as usize) {
-            // FIXME: this looks stupid?
-            let idx = match slot.content {
-                SlotContent::Occupied(_) => self.free_slot,
+            let next = match slot.content {
                 SlotContent::Vacant(idx) => idx,
+                SlotContent::Occupied(_) => self.free_slot,
             };
-            let handle = KeyHandle::new(idx);
-            let (k, v) = f(&handle)?;
-
+            let key = Key::new(self.free_slot, slot.version);
+            let v = f(&key)?;
             slot.content = SlotContent::Occupied(v);
+
             self.count += 1;
+            self.free_slot = next;
 
-            self.inner.push(Slot::vacant(self.count));
-            self.free_slot += 1;
-
-            return Ok(k);
+            return Ok(key);
         }
 
-        let last = self.inner.last().unwrap();
-        let handle = KeyHandle::new(last.next);
-        let (k, v) = f(&handle)?;
-        self.inner.push(Slot::occupied(v, last.next));
+        let key = Key::new(self.free_slot, 0);
+        let v = f(&key)?;
+        self.inner.push(Slot::occupied(v));
+        self.count += 1;
+        self.free_slot += 1;
 
-        Ok(k)
+        Ok(key)
     }
 
-    pub fn get(&self, k: &K) -> Option<&V> {
+    pub fn get(&self, key: &Key<K>) -> Option<&V> {
         self.inner
-            .get(k.idx() as usize)
-            .map(|slot| slot.content.get())
-            .flatten()
+            .get(key.idx() as usize)
+            .and_then(|slot| slot.content.get())
     }
 
-    pub fn get_mut(&mut self, k: &K) -> Option<&mut V> {
+    pub fn get_mut(&mut self, key: &Key<K>) -> Option<&mut V> {
         self.inner
-            .get_mut(k.idx() as usize)
-            .map(|slot| slot.content.get_mut())
-            .flatten()
+            .get_mut(key.idx() as usize)
+            .and_then(|slot| slot.content.get_mut())
+    }
+
+    pub fn contains(&self, key: &Key<K>) -> bool {
+        self.inner
+            .get(key.idx() as usize)
+            .is_some_and(|slot| key.version == slot.version)
+    }
+
+    pub fn remove(&mut self, key: &Key<K>) -> Option<V> {
+        self.remove_with_index(key.idx() as usize)
+    }
+
+    pub fn remove_with_index(&mut self, index: usize) -> Option<V> {
+        if let Some(slot) = self.inner.get_mut(index) {
+            let v = slot.content.swap(self.free_slot);
+            slot.version += 1;
+            self.free_slot = index as u32;
+            self.count -= 1;
+
+            v
+        } else {
+            None
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.inner.clear();
+        self.inner.push(Slot::vacant(1));
+        self.count = 0;
+        self.free_slot = 0;
+    }
+
+    pub fn iter(&self) -> VecMapIterator<'_, K, V> {
+        self.into_iter()
+    }
+}
+
+pub struct VecMapIterator<'a, K: KeyID, V> {
+    inner: &'a VecMap<K, V>,
+    counter: u32,
+}
+
+impl<'a, K: KeyID, V> std::iter::Iterator for VecMapIterator<'a, K, V> {
+    type Item = (Key<K>, &'a V);
+    fn next(&mut self) -> Option<Self::Item> {
+        self.counter += 1;
+        self
+            .inner
+            .inner
+            .get(self.counter as usize - 1)
+            .and_then(|slot| {
+                slot.content
+                    .get()
+                    .map(|v| (Key::new(self.counter, slot.version), v))
+            })
+    }
+}
+
+impl<'a, K: KeyID, V> std::iter::IntoIterator for &'a VecMap<K, V> {
+    type Item = (Key<K>, &'a V);
+    type IntoIter = VecMapIterator<'a, K, V>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        VecMapIterator {
+            inner: self,
+            counter: 0,
+        }
     }
 }
 
 #[cfg(test)]
-mod slot_test {
+mod vecmap {
     use super::*;
 
-    #[derive(Debug)]
+    #[derive(Debug, PartialEq, Eq)]
     struct MyKey(u32);
 
-    impl Key for MyKey {
-        fn idx(&self) -> u32 {
-            self.0
-        }
-
-        fn from_handle(handle: &KeyHandle<Self>) -> Self {
-            Self(handle.idx())
-        }
-    }
+    impl KeyID for MyKey {}
 
     #[test]
-    fn slot_insert() {
-        let mut storage: Map<MyKey, u8> = Map::new();
+    fn insert() {
+        let mut storage: VecMap<MyKey, u8> = VecMap::new();
         let key = storage.insert(69);
-        assert!(key.idx() == 0)
+        assert_eq!(key.idx(), 0)
     }
 
     #[test]
-    fn slot_get() {
-        let mut storage: Map<MyKey, u8> = Map::new();
+    fn get() {
+        let mut storage: VecMap<MyKey, u8> = VecMap::new();
         let key = storage.insert(69);
         let res = storage.get(&key).unwrap();
-        assert!(*res == 70)
+        assert_eq!(*res, 69)
+    }
+
+    #[test]
+    fn remove() {
+        let mut storage: VecMap<MyKey, u8> = VecMap::new();
+        let mut keys = vec![];
+        for i in 0..10u8 {
+            let key = storage.insert(i);
+            keys.push(key);
+        }
+        assert!(storage.inner.last().is_some_and(|slot| *slot.content.get().unwrap() == 9));
+
+        let key = &keys[3];
+        let removed = storage.remove(key);
+        assert_eq!(removed, Some(3));
+
+        let after_remove = storage.get(key);
+        assert_eq!(after_remove, None);
+
+        let new_key = storage.insert(100);
+        assert_ne!(&new_key, key);
+    }
+
+    #[test]
+    fn double_remove() {
+        let mut storage: VecMap<MyKey, u8> = VecMap::new();
+        let mut keys = vec![];
+        for i in 0..10u8 {
+            let key = storage.insert(i);
+            keys.push(key);
+        }
+
+        for key in &keys[1..3] {
+            let _ = storage.remove(key);
+        }
+
+        assert_eq!(storage.free_slot, 2);
+        assert_eq!(storage.inner[1].content, SlotContent::Vacant(10));
+        assert_eq!(storage.inner[2].content, SlotContent::Vacant(1));
+
+        let _ = storage.insert(69);
+        assert_eq!(storage.free_slot, 1);
+
+        let _ = storage.insert(100);
+        assert_eq!(storage.free_slot, 10);
     }
 }
